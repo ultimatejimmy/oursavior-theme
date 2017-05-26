9aaf7965453670027aa8be02c3b3c58e82949ba4
Jimmy Pautz <jpautz@dig.ms>
2017-05-26 10:33:00 -0500

    Add button format to TinyMCE

--- functions.php
+++ functions.php
@@ -235,4 +235,48 @@ add_filter( 'mpp_avatar_override', '__return_true' );
 add_editor_style('https://fonts.googleapis.com/css?family=Josefin+Sans|Lato:400,400i,700');
 add_editor_style('style.css');
 
+
+//Custom styles in TinyMCE
+function wpb_mce_buttons_2($buttons) {
+	array_unshift($buttons, 'styleselect');
+	return $buttons;
+}
+add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
+
+/*
+* Callback function to filter the MCE settings
+*/
+
+function my_mce_before_init_insert_formats( $init_array ) {  
+
+// Define the style_formats array
+
+	$style_formats = array(  
+/*
+* Each array child is a format with it's own settings
+* Notice that each array has title, block, classes, and wrapper arguments
+* Title is the label which will be visible in Formats menu
+* Block defines whether it is a span, div, selector, or inline style
+* Classes allows you to define CSS classes
+* Wrapper whether or not to add a new block-level element around any selected elements
+*/
+		array(  
+			'title' => 'Button',  
+			'block' => 'span',  
+			'classes' => 'button',
+			'wrapper' => false,
+			
+		),  
+		
+	);  
+	// Insert the array, JSON ENCODED, into 'style_formats'
+	$init_array['style_formats'] = json_encode( $style_formats );  
+	
+	return $init_array;  
+  
+} 
+// Attach callback to 'tiny_mce_before_init' 
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' ); 
+
+
 ?>
